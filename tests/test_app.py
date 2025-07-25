43fa0f3fec305fd2c37990f8f6c6b3c81efee25f
Pierre Defourneaux <pierre.defourneaux@gmail.com>
2025-07-25 12:27:36 +0200

    3eme test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,9 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import pytest
 from app import app as flask_app
-
+import requests
 
 @pytest.fixture
 def client():
@@ -14,20 +13,19 @@ def client():
 
 # Assurez vous que la forme de sortie du vectorizer est cohérente avec la forme
 #  d'entrée du classifieur (Logistic regression)
-
 def test_vectorizer_shape(): # pas besoin de client ici car on n'utilise pas de route
     from app import vectorizer, model
     assert vectorizer.get_feature_names_out().shape[0] == model.n_features_in_
 
-
-
 # Assurez-vous que la probabilité de la prédiction de la classe prédite
 # est comprise entre 0 et 1 (en sortie de predict_proba() du classifieur)
-
 def test_check_classifier_output():
     from app import vectorizer, model
     transformed_phrase = vectorizer.transform(["Ceci est une phrase test"])
     probabilities = model.predict_proba(transformed_phrase)[0]
     assert probabilities.sum() == 1
 
-# tester l'intégration avec mise en cache
+# Assurez-vous que la requête GET sur la page d'accueil (route "/") renvoie bien le code HTTP 200.
+def test_code200():
+    response = requests.get("/")
+    assert response.status_code == 200
