51480909c02fe128bf87977ba5bc0e9114b02263
Pierre Defourneaux <pierre.defourneaux@gmail.com>
2025-07-25 12:06:36 +0200

    2eme test

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,17 @@ def client():
 # Assurez vous que la forme de sortie du vectorizer est cohérente avec la forme
 #  d'entrée du classifieur (Logistic regression)
 
-def test_vectorizer_shape():
+def test_vectorizer_shape(): # pas besoin de client ici car on n'utilise pas de route
     from app import vectorizer, model
     assert vectorizer.get_feature_names_out().shape[0] == model.n_features_in_
-     
+
+
+
+# Assurez-vous que la probabilité de la prédiction de la classe prédite
+# est comprise entre 0 et 1 (en sortie de predict_proba() du classifieur)
+
+def check_classifier_output():
+    from app import vectorizer, model
+    transformed_phrase = vectorizer.transform(["Ceci est une phrase test"])
+    probabilities = model.predict_proba(transformed_phrase)[0]
+    assert probabilities.sum() == 1
